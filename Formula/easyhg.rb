9703a80aab551d081e53cb5646ede7a7db231396
shuyang790 <9460985+shuyang790@users.noreply.github.com>
2026-02-14 01:22:22 +0000

    easyhg: bump snapshot to 62af594d627c73c7521cceeb8bed0b8d60026b41

--- Formula/easyhg.rb
+++ Formula/easyhg.rb
@@ -1,8 +1,9 @@
 class Easyhg < Formula
   desc "Lazygit-style terminal UI for Mercurial"
   homepage "https://github.com/shuyang790/EasyHg"
-  url "https://github.com/shuyang790/EasyHg/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "5c2f771110b215071948167df14ec52a8927fdbb65eee9617743a5571fb8c236"
+  url "https://github.com/shuyang790/EasyHg/archive/62af594d627c73c7521cceeb8bed0b8d60026b41.tar.gz"
+  sha256 "91cc249f12b7bba9b46ac4588b2f8b281d612001943b41f0a1e054c615064530"
+  version "0.2.1.dev.20260214.62af594"
   license "MIT"
 
   depends_on "rust" => :build
@@ -12,6 +13,6 @@ class Easyhg < Formula
   end
 
   test do
-    assert_match "0.2.0", shell_output("#{bin}/easyhg --version")
+    assert_match "0.2.1", shell_output("#{bin}/easyhg --version")
   end
 end
